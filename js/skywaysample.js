1e17c69c78a99669b2806092bc463da9d8b131b9
motohacy <kazutaka.motohashi@gmail.com>
2021-01-19 10:42:55 +0900

    test

--- js/skywaysample.js
+++ js/skywaysample.js
@@ -34,7 +34,7 @@ const Peer = window.Peer;
     .catch(console.error);
 
   // Render local stream
-  localVideo.muted = true;
+  localVideo.muted = false;
   localVideo.srcObject = localStream;
   localVideo.playsInline = true;
   await localVideo.play().catch(console.error);
